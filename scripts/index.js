98165b5ba1ad2207f04c5d9a767dd9164b7b74e7
ShumOk <shum_ok@bk.ru>
2021-11-12 23:55:58 +0300

    fix: rew1

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,37 @@
 const popupElement = document.querySelector('.popup'); //переменная всего popup
 const editProfile = document.querySelector('.edit-profile'); //переменная открыть редактирование профиля
 const closeButton = document.querySelector('.popup__close-button'); //переменная закрыть редактирование
-const saveButton = document.querySelector('.popup__save-button'); //переменная кнопки сохранить
+
 const userName = document.querySelector('.profile__user-name'); //переменная касса имени профиля на странице
 const userJop = document.querySelector('.profile__user-jop'); //переменная касса работы на странице
 
-let nameInput = document.querySelector('.popup__user-name'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__user-jop'); // Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector('.popup__input_string_name'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.popup__input_string_jop'); // Воспользуйтесь инструментом .querySelector()
+
+let formElement = document.querySelector('.popup__form'); // Находим форму в DOM
 
 
 function openPopup() {
-  popupElement.classList.remove('popup_opened');
+  popupElement.classList.add('popup_opened');
+  nameInput.value = userName.textContent;
+  jobInput.value = userJop.textContent;
 }
 
 function closePopup() {
-  popupElement.classList.add('popup_opened');
+  popupElement.classList.remove('popup_opened');
 }
 
-editProfile.addEventListener('click', openPopup)
-closeButton.addEventListener('click', closePopup)
 
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
-nameInput.value = userName.textContent;
-jobInput.value = userJop.textContent;
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) {
   evt.preventDefault();
 
-  // Получите значение полей из свойства value
-  userName.textContent = nameInput.value;
+  userName.textContent = nameInput.value; // Получите значение полей из свойства value
   userJop.textContent = jobInput.value;
 
   closePopup();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler); // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+
+editProfile.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
